dcce55002aa045fc8ca954ab7b7c569550929186
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2020-09-15 21:58:35 +0200

    2: Introduction with dummy test

---
+++ test/start.js
@@ -0,0 +1,13 @@
+const expect = require("chai").expect;
+
+it("should add numbers correctly", function () {
+  const a = 3,
+    b = 2;
+  expect(a + b).to.equal(5);
+});
+
+it("should not give a result of 5", function () {
+  const a = 3,
+    b = 2;
+  expect(a + b).not.to.equal(5);
+});
